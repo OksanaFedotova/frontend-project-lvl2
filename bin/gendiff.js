978ab0dde53eaff574aca85ce21cf75ebd18fcae
Oksana Fedotova <oxana-fedotova@yandex.ru>
2021-03-10 12:27:21 +0300

    second step

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -3,5 +3,7 @@
 import commander from 'commander';
 commander
 .version('0.0.1')
+.arguments('<filepath1> <filepath2>')
+.option('-f, --format [type]', 'output format')
 .description('Compares two configuration files and shows a difference.');
 commander.parse();
