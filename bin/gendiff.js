f5d91371cbafe80128989910c1ee06c7a85aceab
Oksana Fedotova <oxana-fedotova@yandex.ru>
2021-03-18 14:37:05 +0300

    bug fixed

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,10 +1,12 @@
 #!/usr/bin/env node
 
 import commander from 'commander';
+import genDiff from '../src/gd.js';
 
 commander
   .version('0.0.1')
   .arguments('<filepath1> <filepath2>')
   .option('-f, --format [type]', 'output format')
-  .description('Compares two configuration files and shows a difference.');
+  .description('Compares two configuration files and shows a difference.')
+  .action(genDiff);
 commander.parse();
