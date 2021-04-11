b76197f63ba636e99c53cd9cb6be72fa2cb3b57a
Oksana Fedotova <oxana-fedotova@yandex.ru>
2021-04-11 13:06:49 +0300

    refactoring

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -2,22 +2,37 @@ import fs from 'fs';
 import { fileURLToPath } from 'url';
 import { dirname, join } from 'path';
 import { describe, expect, test } from '@jest/globals';
-import genDiff from '../src/getDiff.js';
+import genDiff from '../src/genDiff.js';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 const getFixturePath = (filename) => join(__dirname, '..', '__tests__', '__fixtures__', filename);
 const readFile = (filename) => fs.readFileSync(getFixturePath(filename), 'utf-8');
 
-const outputFormats = [['default'], ['stylish'], ['plain'], ['json']];
-const inputData = [['Flat', 'json'], ['Tree', 'json'], ['Flat', 'yml'], ['Tree', 'yml']];
+const flatStylish = readFile('outputFlat_stylish.txt');
+const treeStylish = readFile('outputTree_stylish.txt');
+const flatPlain = readFile('outputFlat_plain.txt');
+const treePlain = readFile('outputTree_plain.txt');
+const flatJson = readFile('outputFlat_json.txt');
+const treeJson = readFile('outputTree_json.txt');
 
-describe.each(outputFormats)('genDiff for %s', (outputFormat) => {
-  test.each(inputData)('%s', (nesting, extension) => {
-    const file1 = getFixturePath(`file1${nesting}.${extension}`);
-    const file2 = getFixturePath(`file2${nesting}.${extension}`);
-    const formatter = outputFormat === 'default' ? '' : outputFormat;
-    const output = readFile(`output${nesting}_${formatter}.txt`);
-    expect(genDiff(file1, file2, formatter)).toEqual(output);
+const outputs = [['default', flatStylish, treeStylish], ['stylish', flatStylish, treeStylish], ['plain', flatPlain, treePlain], ['json', flatJson, treeJson]];
+
+const extensions = ['json', 'yml'];
+
+const nestingTypes = [['Flat'], ['Tree']];
+
+describe.each(outputs)('genDiff for %s', (name, Flat, Tree) => {
+  describe.each(nestingTypes)('%s', (nestingType) => {
+    test.each(extensions)('%s', (extension) => {
+      const file1 = getFixturePath(`file1${nestingType}.${extension}`);
+      const file2 = getFixturePath(`file2${nestingType}.${extension}`);
+      const formatter = name === 'default' ? '' : name;
+      if (nestingType === 'Flat') {
+        expect(genDiff(file1, file2, formatter)).toEqual(Flat);
+      } else {
+        expect(genDiff(file1, file2, formatter)).toEqual(Tree);
+      }
+    });
   });
 });
